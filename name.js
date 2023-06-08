0f87648edeac06ca11c37df22d6ba08e29625322
joshuamayowa <abegundejoshu@gmail.com>
2023-06-08 16:19:56 +0100

    feat: create java

---
+++ name.js
@@ -0,0 +1,26 @@
+// let myName= 'Joshua'
+// myName= 'mayor'
+// const school= "univelcity"
+// const state= `lagos`
+// const statement= ` My name  is   ${myName}
+//  am a student of  ${school}  and am from   ${state}`  
+// const statement2= "Mohammed Ali said 'suffer now and live the rest of your life a campion'"
+
+
+// // let age= prompt('how old are you')
+
+// // console.log(`you were born  in ${new Date().getFullYear() - age}`)
+
+
+let good= prompt('what is your name')
+let book= prompt( 'what year were you born?') 
+let statement3= `Your nameis ${good} is 
+ and you are ${new Date().getFullYear() - book} years 
+  old`
+console.log(statement3)
+
+
+
+// alert(statement2)
+
+// confirm(statement)
